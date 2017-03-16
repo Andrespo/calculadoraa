2f0405ea79707d57a13cdbfa0993994b2c82c10d
Andrespo <stalybskt@hotmail.com>
2017-03-16 18:04:37 -0500

    Add files via upload

---
+++ Calculador/Calculador/src/calculador/Calculador.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculador {
+
+    
+    public double sumar(double numero1 , double numero2){
+       
+    double suma;
+    suma=0;
+    suma=numero1+numero2;
+    return suma;
+    }
+    public Double restar(double numero1 , double numero2){
+        
+    double resta;
+    resta=0;
+    resta=numero1 + numero2;
+    return resta;
+      
+    }
+    public Double multiplicar(double numero1 , double numero2){
+        
+    double multiplicacion;
+    multiplicacion=0;
+    multiplicacion=numero1 + numero2;
+    return multiplicacion;
+    }
+    public Double dividir(double numero1 , double numero2){
+    double division;
+    division=0;
+    division=numero1 + numero2;
+    return division;
+    }
+       
+    
+    }
+    
---
+++ Calculador/Calculador/src/calculador/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculador;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Calculador miCalculadorPrincipal= new Calculador();
+        
+        Consola miConsola= new Consola();
+        miConsola.calculadorcon= miCalculadorPrincipal;
+        miConsola.Operaciones();
+        
+               
+        
+    }
+        
+        
+    }
+
